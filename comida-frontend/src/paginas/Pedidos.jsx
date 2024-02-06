26be6c3b0d53c8ec13c5446470adc9461224274f
Matías Campos <matias98cs@gmail.com>
2024-02-05 23:20:53 -0300

    interno

--- comida-frontend/src/paginas/Pedidos.jsx
+++ comida-frontend/src/paginas/Pedidos.jsx
@@ -1,12 +1,23 @@
 import PedidosTable from "../componentes/pedidos/PedidosTable"
+import SpinnerComponent from "../componentes/spinner/SpinnerComponent"
+import useComidas from "../hooks/useComidas"
 import '../style/pedidos.css'
 
 const Pedidos = () => {
+  const {pedidos} = useComidas()
   return (
     <div className='pedido-container'>
       <h1>Últimos Pedidos Tomados</h1>
       <div className="pedido-table">
-        <PedidosTable/>
+        {
+          pedidos?.length > 1
+          ?
+          <PedidosTable/>
+          :
+          <div>
+            <SpinnerComponent/>
+          </div>
+        }
       </div>
     </div>
   )
